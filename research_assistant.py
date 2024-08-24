77cd66ba3cd6fc2d6e14d3b13fea6370508ec5f3
Raihan Khan <iamrk98@gmail.com>
2024-08-24 20:19:26 +0530

    env loaded

--- research_assistant.py
+++ research_assistant.py
@@ -2,7 +2,10 @@ from phi.assistant import Assistant
 from phi.llm.groq import Groq
 from phi.tools.duckduckgo import DuckDuckGo
 from phi.tools.file import FileTools
+from dotenv import load_dotenv
 from prompts import description, instructions, query
+
+load_dotenv()
 
 assistant = Assistant(
     llm=Groq(model="llama-3.1-8b-instant"),
